1e86c8a79f715da8857a14c94373da097602a93c
Addison Klinke <addison@openalpr.com>
2019-06-06 12:13:46 -0600

    add sleep condition to while-loop when queue is empty

--- speed-bench/alprbench.py
+++ speed-bench/alprbench.py
@@ -2,9 +2,12 @@ import argparse
 from multiprocessing import cpu_count
 import os
 import platform
+import re
+from threading import Thread, Lock
+from time import time, sleep
 import urllib
-from openalpr import Alpr
 from alprstream import AlprStream
+from openalpr import Alpr
 
 
 class AlprBench:
@@ -17,25 +20,35 @@ class AlprBench:
     :param str config: Path to OpenALPR configuration file.
     :param bool quiet: Suppress all output besides final results.
     """
-
     def __init__(self, streams, resolution, downloads='/tmp/alprbench', runtime=None, config=None, quiet=False):
 
         # Transfer parameters to attributes
-        self.streams = streams
         self.quiet = quiet
-        if resolution == 'all':
-            self.resolution = ['vga', '720p', '1080p', '4k']
-        elif isinstance(resolution, str):
-            self.resolution = [resolution]
+        self.message('Initializing...')
+        self.streams = streams
+        if isinstance(resolution, str):
+            if resolution == 'all':
+                self.resolution = ['vga', '720p', '1080p', '4k']
+            else:
+                self.resolution = [resolution]
+        elif isinstance(resolution, list):
+            self.resolution = resolution
+        else:
+            raise ValueError('Expected list or str for resolution, but received {}'.format(resolution))
         self.downloads = downloads
         if not os.path.exists(self.downloads):
             os.mkdir(self.downloads)
+        self.threads_active = False
+        self.frame_counter = 0
+        self.mutex = Lock()
 
         # Detect operating system
         if platform.system().lower().find('linux') == 0:
             operating = 'linux'
+            self.message('\tOperating system: Linux')
         elif platform.system().lower().find('windows') == 0:
             operating = 'windows'
+            self.message('\tOperating system: Windows')
         else:
             raise OSError('Detected OS other than Linux or Windows')
 
@@ -48,12 +61,37 @@ class AlprBench:
             self.config = '/usr/share/openalpr/config/openalpr.defaults.conf'
             if operating == 'windows':
                 self.config = 'C:/OpenALPR/Agent' + self.config
-        self.message('Runtime data: {}'.format(self.runtime))
-        self.message('OpenALPR configuration: {}'.format(self.config))
+        self.message('\tRuntime data: {}'.format(self.runtime))
+        self.message('\tOpenALPR configuration: {}'.format(self.config))
 
     def __call__(self):
+        """Run threaded benchmarks on all requested resolutions."""
         videos = self.download_benchmarks()
-        alprs, alprstreams = self.init_alpr()
+        alprstream = AlprStream(frame_queue_size=10)
+        name_regex = re.compile('(?<=\/)[^\.\/]+')
+        self.threads_active = True
+        print('Benchmarking on {} threads...'.format(cpu_count()))
+
+        for v in videos:
+            self.frame_counter = 0
+            threads = []
+            alprstream.connect_video_file(v, 0)
+            for i in range(cpu_count()):
+                threads.append(Thread(target=self.worker, args=(alprstream, )))
+                threads[i].setDaemon(True)
+            start = time()
+            for t in threads:
+                t.start()
+            while len(threads) > 0:
+                try:
+                    threads = [t.join() for t in threads if t is not None and t.isAlive()]
+                except KeyboardInterrupt:
+                    print('\n\nCtrl-C received! Sending kill to threads...')
+                    self.threads_active = False
+                    break
+            elapsed = time() - start
+            print('\t{} = {:.1f} fps ({} frames)'.format(
+                name_regex.findall(v)[-1], self.frame_counter/elapsed, self.frame_counter))
 
     def download_benchmarks(self):
         """Save requested benchmark videos locally.
@@ -64,29 +102,46 @@ class AlprBench:
         endpoint = 'http://download.openalpr.com/bench'
         files = ['vga.webm', '720p.mp4', '1080p.mp4', '4k.mp4']
         existing = os.listdir(self.downloads)
+        self.message('Downloading benchmark videos...')
         for f in files:
             short = f.split('.')[0]
             if short in self.resolution:
                 out = os.path.join(self.downloads, f)
                 videos.append(out)
                 if f not in existing:
-                    _ = urllib.request.urlretrieve(os.path.join(endpoint, f), out)
-                    self.message('Downloaded {}'.format(short))
+                    _ = urllib.urlretrieve(os.path.join(endpoint, f), out)
+                    self.message('\tDownloaded {}'.format(short))
                 else:
-                    self.message('{} already exists'.format(short))
+                    self.message('\tFound local {}'.format(short))
         return videos
 
-    def init_alpr(self):
-        """Load Alpr and AlprStream objects into memory."""
-        alprs = []
-        alprstreams = []
-        cores = cpu_count()
-        alpr = Alpr('us', '', '')
-
     def message(self, msg):
-        """Control verbosity of output."""
+        """Control verbosity of output.
+
+        :param str msg: Message to display.
+        :return: None
+        """
         if not self.quiet:
             print(msg)
+
+    def worker(self, alprstream):
+        """Worker thread for a single stream and Alpr combination.
+
+        :param alprstream: OpenALPR instance to manage the video stream.
+        :return: None
+        """
+        alpr = Alpr('us', self.config, self.runtime)
+        while alprstream.video_file_active() or alprstream.get_queue_size() > 0:
+            if not self.threads_active:
+                break
+            if alprstream.get_queue_size() == 0:
+                sleep(0.1)
+                continue
+            _ = alprstream.pop_completed_groups()
+            _ = alprstream.process_frame(alpr)
+            self.mutex.acquire()
+            self.frame_counter += 1
+            self.mutex.release()
 
 
 if __name__ == '__main__':
@@ -96,12 +151,19 @@ if __name__ == '__main__':
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-d', '--download_dir', type=str, default='/tmp/alprbench', help='folder to save videos')
     parser.add_argument('-q', '--quiet', action='store_true', help='suppress all output besides final results')
-    parser.add_argument('-r', '--resolution', type=str, default='all', choices=['vga', '720p', '1080p', '4k'],
-                        help='video resolution to benchmark on')
+    parser.add_argument('-r', '--resolution', type=str, default='all', help='video resolution to benchmark on')
     parser.add_argument('-s', '--streams', type=int, default=1, help='number of camera streams to simulate')
     parser.add_argument('--config', type=str, help='path to OpenALPR config, detects Windows/Linux and uses defaults')
     parser.add_argument('--runtime', type=str, help='path to runtime data, detects Windows/Linux and uses defaults')
     args = parser.parse_args()
 
-    bench = AlprBench(args.streams, args.resolution, args.download_dir, args.runtime, args.config, args.quiet)
+    if ',' in args.resolution:
+        args.resolution = [r.strip() for r in args.resolution.split(',')]
+    bench = AlprBench(
+        args.streams,
+        args.resolution,
+        args.download_dir,
+        args.runtime,
+        args.config,
+        args.quiet)
     bench()
